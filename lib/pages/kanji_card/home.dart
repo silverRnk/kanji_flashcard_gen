8c11a2074c0ce986c5fa89d89fd451779b761c9b
patrick <patdi.bautista@gmail.com>
2023-12-20 00:18:09 +0800

    add route to add card

--- lib/pages/kanji_card/home.dart
+++ lib/pages/kanji_card/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kanji_flashcard_gen/pages/kanji_card/add_card.dart';
 import 'package:kanji_flashcard_gen/widget/kanji_chips.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,12 +12,27 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Wrap(
-        children: [
-          KanjiChip(),
-          KanjiChip()
-        ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                return AddCardPage();
+              },))
+            }, icon: Icon(Icons.add), tooltip: 'Create a new card',)
+          ],
+        ),
+        body: Stack(
+          children: [Wrap(
+            spacing: 10,
+            children: [
+              KanjiChip(),
+              KanjiChip(),
+              MyInkWellChip()
+            ],
+          ),]
+        ),
       ),
     );
   }
--- lib/widget/kanji_chips.dart
+++ lib/widget/kanji_chips.dart
@@ -1,30 +1,80 @@
 import 'package:flutter/material.dart';
 
-class KanjiChip extends StatelessWidget {
-  const KanjiChip({super.key});
+class KanjiChip extends StatefulWidget{
+
+  KanjiChip({super.key});
+
+  @override
+  State<StatefulWidget> createState() =>  _KanjiChipState();
+}
+
+class _KanjiChipState extends State<KanjiChip> {
+  OverlayPortalController _overlayPortalController = new OverlayPortalController();
+
+  Widget _myInkWellContainer({required Widget child, Color? color, void Function()? onTap }){
+    return Container(
+      decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(blurRadius: 2.0)
+          ]
+        ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: onTap,
+          child: child
+        )), 
+      );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
+    return _myInkWellContainer(
+      color: Colors.white,
       onTap: () {
         debugPrint('Selected an kanji card');
       },
       child: Container(
         height: 65,
         width: 250,
-        margin: EdgeInsets.all(10),
         padding: const EdgeInsets.fromLTRB(10, 10, 5, 10),
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-          boxShadow: [
-            BoxShadow(blurRadius: 2.0)
-          ]
-        ),
         child: Row(
-          children: [Expanded(child: Text('Kanji')), IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))],
+          children: [Expanded(child: Text('Kanji')), MenuAnchor(builder: (context, controller, child) {
+            return IconButton(onPressed: () {
+              if(!controller.isOpen){
+                controller.open();
+              }else{
+                controller.close();
+              }
+            }, icon: Icon(Icons.more_vert), tooltip: 'Show more',);
+          },menuChildren: List<MenuItemButton>.generate(3, 
+          (index) => MenuItemButton(child: Text('Selection $index'), onPressed: () {},)))],
         ),
       ),
+    );
+  }
+}
+
+
+class MyInkWellChip extends StatelessWidget {
+  const MyInkWellChip({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      child:  Material(
+        child:  InkWell(
+          onTap: (){print("tapped");},
+          child: Container(
+            width: 100.0,
+            height: 100.0,
+          ),
+        ),
+        color: Colors.transparent,
+      ),
+      color: Colors.orange,
     );
   }
 }
